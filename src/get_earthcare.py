46f7193fbaef76ac4cc4cbe1513af2c4b42d5c8f
Anqi Li <li.a.titech@gmail.com>
2025-06-30 12:42:51 +0200

    get the right units using arts functions

--- src/get_earthcare.py
+++ src/get_earthcare.py
@@ -7,6 +7,9 @@ from combine_CPR_MSI import (
     align_xmet_horizontal_grid,
 )
 import numpy as np
+from tqdm import tqdm
+from pathlib import Path
+from physics.unitconv import mixing_ratio2mass_conc, dry_air_density
 
 
 # %%
@@ -79,41 +82,66 @@ def prepare_arts(
     # make liquid water content
     # assuming dry air, ideal gas law, and constant specific gas constant for air
     # R = 287 J/(kg K) = 287e3 m^2
-    ds_xmet_height = ds_xmet_height.eval("density = pressure/(temperature * 287e3)")
+    # ds_xmet_height = ds_xmet_height.eval("density = pressure/(temperature * 287e3)")
+    ds_xmet_height["density"] = dry_air_density(
+        ds_xmet_height["pressure"],
+        ds_xmet_height["temperature"],
+    )
     ds_xmet_height["density"] = ds_xmet_height["density"].assign_attrs(
         dict(
             long_name="Air Density",
             units="kg m-3",
         )
     )
-    ds_xmet_height = ds_xmet_height.eval(
-        "cloud_liquid_water_content = specific_cloud_liquid_water_content * density"
+    # ds_xmet_height = ds_xmet_height.eval(
+    #     "cloud_liquid_water_content = specific_cloud_liquid_water_content * density"
+    # )
+    ds_xmet_height["cloud_liquid_water_content"] = mixing_ratio2mass_conc(
+        ds_xmet_height["specific_cloud_liquid_water_content"],  # kg/kg
+        ds_xmet_height["pressure"],  # Pa
+        ds_xmet_height["temperature"],  # K
+    ).assign_attrs(
+        dict(
+            long_name="Cloud Liquid Water Content",
+            units="kg m-3",
+        )
     )
+
     ds_xmet_height["cloud_liquid_water_content"] = (
         ds_xmet_height["cloud_liquid_water_content"]
-        .assign_attrs(
-            dict(
-                long_name="Cloud Liquid Water Content",
-                units="kg m-3",
-            )
-        )
+        # .assign_attrs(
+        #     dict(
+        #         long_name="Cloud Liquid Water Content",
+        #         units="kg m-3",
+        #     )
+        # )
         .where(
             ds_xmet_height["cloud_liquid_water_content"] >= 0,
             0,  # ensure no negative values
         )
     )
 
-    ds_xmet_height = ds_xmet_height.eval(
-        "rain_water_content = specific_rain_water_content * density"
+    # ds_xmet_height = ds_xmet_height.eval(
+    #     "rain_water_content = specific_rain_water_content * density"
+    # )
+    ds_xmet_height["rain_water_content"] = mixing_ratio2mass_conc(
+        ds_xmet_height["specific_rain_water_content"],  # kg/kg
+        ds_xmet_height["pressure"],  # Pa
+        ds_xmet_height["temperature"],  # K
+    ).assign_attrs(
+        dict(
+            long_name="Rain Liquid Water Content",
+            units="kg m-3",
+        )  # kg/m^3 = kg/kg * kg/m^3
     )
     ds_xmet_height["rain_water_content"] = (
         ds_xmet_height["rain_water_content"]
-        .assign_attrs(
-            dict(
-                long_name="Rain Liquid Water Content",
-                units="kg m-3",
-            )
-        )
+        # .assign_attrs(
+        #     dict(
+        #         long_name="Rain Liquid Water Content",
+        #         units="kg m-3",
+        #     )
+        # )
         .where(
             ds_xmet_height["rain_water_content"] >= 0,
             0,  # ensure no negative values
@@ -132,12 +160,6 @@ def prepare_arts(
         (xds["pixel_values"] > 500), (xds["pixel_values"] < 150)
     )
 
-    # # combine all masks
-    # if remove_clearsky:
-    #     # mask clearsky profiles
-    #     mask_clearsky = (da_dBZ_height < lowest_dBZ_threshold).all(dim="height_grid")
-    #     mask = np.logical_and(~mask_clearsky, ~mask_bad_msi)
-    # else:
     mask = ~mask_bad_msi
 
     # % combine all necessary variables
@@ -159,12 +181,67 @@ def prepare_arts(
 # %%
 if __name__ == "__main__":
     # %%
-    orbit_frame = "01162E"
-    ds = prepare_arts(orbit_frame, remove_clearsky=False)
+    # orbit_frame = "01162E"
+    for orbit_frame in tqdm(
+        [
+            "01162E",
+            "04090A",
+            "04758A",
+            "03994G",
+            "04142B",
+            "04937G",
+            "05248E",
+            "04688D",
+        ],
+        desc="Processing orbits",
+    ):
 
-    # %% save ds to netcdf file
-    path_save = "../data/earthcare/arts_input_data/"
-    ds.reset_index("nray").set_xindex("time").to_netcdf(
-        path_save + f"arts_input_{orbit_frame}.nc",
-        compute=True,
-    )
+        path_save = "../data/earthcare/arts_input_data2/"
+        filename_save = f"arts_input_{orbit_frame}.nc"
+        filepath = Path(path_save) / filename_save
+        if filepath.exists():
+            print(f"File {filename_save} already exists, skipping.")
+            continue
+        ds = prepare_arts(orbit_frame, remove_clearsky=False)
+        ds.reset_index("nray").set_xindex("time").to_netcdf(
+            path_save + filename_save,
+            compute=True,
+        )
+
+# %%
+from matplotlib import pyplot as plt
+
+orbits = [
+    "01162E",
+    "04090A",
+    "04758A",
+    "03994G",
+    "04142B",
+    "04937G",
+    "05248E",
+    "04688D",
+]
+path = "../data/earthcare/arts_input_data2/"
+ds = xr.open_mfdataset(
+    path + "arts_input_*.nc",
+    combine="nested",
+    concat_dim="nray",
+)
+ds.cloud_liquid_water_content.pipe(np.log10).assign_attrs(
+    units="Log10(kgm-3)"
+).plot.hist(
+    bins=100,
+    range=(-10, 0),
+    density=True,
+)
+# ds.rain_water_content.pipe(np.log10).plot.hist(
+#     bins=100,
+#     range=(-10, 0),
+#     density=True,
+# )
+plt.title(
+    f"""
+          Log10 of Cloud Liquid Water Content
+from orbits: {orbits}
+"""
+)
