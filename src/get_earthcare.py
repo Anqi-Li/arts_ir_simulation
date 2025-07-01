f7d9122d5801a7aa5658a403c2f39288e086d49c
Anqi Li <li.a.titech@gmail.com>
2025-07-01 14:08:24 +0200

    correct temperature interpolation and calculate h20 vmr

--- src/get_earthcare.py
+++ src/get_earthcare.py
@@ -6,10 +6,17 @@ from combine_CPR_MSI import (
     xr_vectorized_height_interpolation,
     align_xmet_horizontal_grid,
 )
+from search_orbit_files import get_common_orbits
 import numpy as np
 from tqdm import tqdm
 from pathlib import Path
-from physics.unitconv import mixing_ratio2mass_conc, dry_air_density
+from physics.unitconv import (
+    mixing_ratio2mass_conc,
+    dry_air_density,
+    specific_humidity2h2o_p,
+)
+import os
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 
 # %%
@@ -67,22 +74,41 @@ def prepare_arts(
         height_dim="height",
         new_height_dim="height_grid",
     )
-    ds_xmet_height[["log_pressure", "temperature"]] = ds_xmet_height[
-        ["log_pressure", "temperature"]
-    ].interpolate_na(
+    ds_xmet_height["log_pressure"] = ds_xmet_height["log_pressure"].interpolate_na(
         dim="height_grid",
         method="linear",
         fill_value="extrapolate",
     )
     ds_xmet_height["pressure"] = ds_xmet_height["log_pressure"].pipe(np.exp)
 
+    ds_xmet_height["temperature"] = ds_xmet_height["temperature"].interpolate_na(
+        dim="height_grid",
+        method="nearest",
+        fill_value="extrapolate",
+    )
+    # % assign attributes to the interpolated variables
     for v in vars_xmet:
         ds_xmet_height[v] = ds_xmet_height[v].assign_attrs(ds_xmet[v].attrs)
 
+    # make H2O VMR from specific_humidity
+    ds_xmet_height["h2o_volume_mixing_ratio"] = (
+        (
+            specific_humidity2h2o_p(
+                ds_xmet_height["specific_humidity"],
+                ds_xmet_height["pressure"],
+            )
+            / ds_xmet_height["pressure"]
+        )
+        .fillna(0)
+        .assign_attrs(
+            dict(
+                long_name="Water Vapor Volume Mixing Ratio",
+                units="kg kg-1",
+            )
+        )
+    )
+
     # make liquid water content
-    # assuming dry air, ideal gas law, and constant specific gas constant for air
-    # R = 287 J/(kg K) = 287e3 m^2
-    # ds_xmet_height = ds_xmet_height.eval("density = pressure/(temperature * 287e3)")
     ds_xmet_height["density"] = dry_air_density(
         ds_xmet_height["pressure"],
         ds_xmet_height["temperature"],
@@ -93,9 +119,7 @@ def prepare_arts(
             units="kg m-3",
         )
     )
-    # ds_xmet_height = ds_xmet_height.eval(
-    #     "cloud_liquid_water_content = specific_cloud_liquid_water_content * density"
-    # )
+
     ds_xmet_height["cloud_liquid_water_content"] = mixing_ratio2mass_conc(
         ds_xmet_height["specific_cloud_liquid_water_content"],  # kg/kg
         ds_xmet_height["pressure"],  # Pa
@@ -107,23 +131,13 @@ def prepare_arts(
         )
     )
 
-    ds_xmet_height["cloud_liquid_water_content"] = (
-        ds_xmet_height["cloud_liquid_water_content"]
-        # .assign_attrs(
-        #     dict(
-        #         long_name="Cloud Liquid Water Content",
-        #         units="kg m-3",
-        #     )
-        # )
-        .where(
-            ds_xmet_height["cloud_liquid_water_content"] >= 0,
-            0,  # ensure no negative values
-        )
+    ds_xmet_height["cloud_liquid_water_content"] = ds_xmet_height[
+        "cloud_liquid_water_content"
+    ].where(
+        ds_xmet_height["cloud_liquid_water_content"] >= 0,
+        0,  # ensure no negative values
     )
 
-    # ds_xmet_height = ds_xmet_height.eval(
-    #     "rain_water_content = specific_rain_water_content * density"
-    # )
     ds_xmet_height["rain_water_content"] = mixing_ratio2mass_conc(
         ds_xmet_height["specific_rain_water_content"],  # kg/kg
         ds_xmet_height["pressure"],  # Pa
@@ -134,24 +148,18 @@ def prepare_arts(
             units="kg m-3",
         )  # kg/m^3 = kg/kg * kg/m^3
     )
-    ds_xmet_height["rain_water_content"] = (
-        ds_xmet_height["rain_water_content"]
-        # .assign_attrs(
-        #     dict(
-        #         long_name="Rain Liquid Water Content",
-        #         units="kg m-3",
-        #     )
-        # )
-        .where(
-            ds_xmet_height["rain_water_content"] >= 0,
-            0,  # ensure no negative values
-        )
+    ds_xmet_height["rain_water_content"] = ds_xmet_height["rain_water_content"].where(
+        ds_xmet_height["rain_water_content"] >= 0,
+        0,  # ensure no negative values
     )
     vars_xmet = [
         v.replace(
             "specific_cloud_liquid_water_content", "cloud_liquid_water_content"
         ).replace("specific_rain_water_content", "rain_water_content")
         for v in vars_xmet
+    ] + [
+        "h2o_volume_mixing_ratio",
+        "density",
     ]
 
     # % filter bad pixels, cleary sky, etc.
@@ -177,71 +185,37 @@ def prepare_arts(
 
     return ds
 
-
 # %%
 if __name__ == "__main__":
-    # %%
-    # orbit_frame = "01162E"
-    for orbit_frame in tqdm(
-        [
-            "01162E",
-            "04090A",
-            "04758A",
-            "03994G",
-            "04142B",
-            "04937G",
-            "05248E",
-            "04688D",
-        ],
-        desc="Processing orbits",
-    ):
+    # % get common orbits
+    orbit_list = get_common_orbits(["CPR", "MSI", "XMET"])
 
-        path_save = "../data/earthcare/arts_input_data2/"
+    # %%
+    def process_orbit(orbit_frame):
+        path_save = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            "data/earthcare/arts_input_data/",
+        )
         filename_save = f"arts_input_{orbit_frame}.nc"
         filepath = Path(path_save) / filename_save
         if filepath.exists():
-            print(f"File {filename_save} already exists, skipping.")
-            continue
-        ds = prepare_arts(orbit_frame, remove_clearsky=False)
+            return f"File {filename_save} already exists, skipping."
+        ds = prepare_arts(orbit_frame, remove_clearsky=True)
         ds.reset_index("nray").set_xindex("time").to_netcdf(
             path_save + filename_save,
             compute=True,
         )
+        return f"Processed {filename_save}"
+
+    with ProcessPoolExecutor(max_workers=64) as executor:
+        futures = {
+            executor.submit(process_orbit, orbit_frame): orbit_frame
+            for orbit_frame in orbit_list
+        }
+        for f in tqdm(
+            as_completed(futures), total=len(futures), desc="Processing orbit"
+        ):
+            result = f.result()
+            print(result)
 
 # %%
-from matplotlib import pyplot as plt
-
-orbits = [
-    "01162E",
-    "04090A",
-    "04758A",
-    "03994G",
-    "04142B",
-    "04937G",
-    "05248E",
-    "04688D",
-]
-path = "../data/earthcare/arts_input_data2/"
-ds = xr.open_mfdataset(
-    path + "arts_input_*.nc",
-    combine="nested",
-    concat_dim="nray",
-)
-ds.cloud_liquid_water_content.pipe(np.log10).assign_attrs(
-    units="Log10(kgm-3)"
-).plot.hist(
-    bins=100,
-    range=(-10, 0),
-    density=True,
-)
-# ds.rain_water_content.pipe(np.log10).plot.hist(
-#     bins=100,
-#     range=(-10, 0),
-#     density=True,
-# )
-plt.title(
-    f"""
-          Log10 of Cloud Liquid Water Content
-from orbits: {orbits}
-"""
-)
