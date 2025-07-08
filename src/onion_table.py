f92c85821e487a18e2a023acef947fc853a48ca2
Anqi Li <li.a.titech@gmail.com>
2025-07-08 17:05:11 +0200

    allowing exponential psd when creating onion inversion table

--- src/onion_table.py
+++ src/onion_table.py
@@ -14,7 +14,8 @@ from select_habits import get_habit_list_from_path
 
 # %%
 
-def make_onion_invtable(habit, psd):
+
+def make_onion_invtable(habit, psd, kwargs=None):
     """
     Create a radar onion inversion table for the given habit and PSD.
     """
@@ -24,9 +25,10 @@ def make_onion_invtable(habit, psd):
     # Use the standard ARTS habit folder for MW regime
     ws.stdhabits_folder = "/scratch/patrick/Data/StandardHabits"
     # Check if the habit is available
-    if habit not in get_habit_list_from_path(str(ws.stdhabits_folder), suffix= '.xml.bin'):
+    if habit not in get_habit_list_from_path(
+        str(ws.stdhabits_folder), suffix=".xml.bin"
+    ):
         raise ValueError(f"Habit {habit} not found in standard habits.")
-
 
     # We must define a RWC species, even though we will not use it
     ea.scat_speciesAbelBoutle12(ws, "RWC")
@@ -39,13 +41,21 @@ def make_onion_invtable(habit, psd):
     # Set ice PSD
     if psd == "DelanoeEtAl14":
         ea.scat_speciesDelanoeEtAl14(ws, name)
-        
+
     elif psd == "FieldEtAl07TR":
         ea.scat_speciesFieldEtAl07(ws, name, regime="TR")
-        
+
     elif psd == "FieldEtAl07ML":
         ea.scat_speciesFieldEtAl07(ws, name, regime="ML")
-        
+
+    elif psd == "Exponential":
+        # Define an exponential PSD with varing lambda
+        if kwargs is None:
+            raise ValueError("kwargs must be provided for Exponential PSD")
+        n0 = kwargs.get("n0", 1e6) 
+        ga = kwargs.get("ga", 1) 
+        ea.scat_speciesMgdMass(ws, name, n0=n0, mu=0, la=-999, ga=ga)
+
     else:
         raise ValueError(f"PSD {psd} not handled")
 
@@ -63,7 +73,7 @@ def make_onion_invtable(habit, psd):
     ws.VectorCreate("onion_t_grid")
     #
     ws.VectorLinSpace(ws.onion_dbze_grid, -35, 20, 1)
-    ws.VectorLinSpace(ws.onion_t_grid, 180, 273, 1) #neglect temperatures above 0 C
+    ws.VectorLinSpace(ws.onion_t_grid, 180, 273, 1)  # neglect temperatures above 0 C
     #
     ws.RadarOnionPeelingTableCalc(
         invtable=ws.onion_invtable,
@@ -73,60 +83,63 @@ def make_onion_invtable(habit, psd):
         dbze_grid=ws.onion_dbze_grid,
         t_grid=ws.onion_t_grid,
     )
-    
+
     return ws
+
 
 # %% table in a data array and extrapolate to a wider dbz range
 def get_ds_table(habit, psd, ws):
     ds_table = xr.DataArray(
-    name=f"onion_invtable_{habit}_{psd}",
-    data=ws.onion_invtable.value[1].data,
-    coords={
-        "radiative_properties": ["FWC", "Extinction"],
-        "dBZ": ws.onion_dbze_grid.value,
-        "Temperature": ws.onion_t_grid.value,
-    },
-    attrs={
-        "unit": "log10(FWC/[kg/m3])",
-        "description": "Radar onion inversion table for FWC",
-    },
-)
+        name=f"onion_invtable_{habit}_{psd}",
+        data=ws.onion_invtable.value[1].data,
+        coords={
+            "radiative_properties": ["FWC", "Extinction"],
+            "dBZ": ws.onion_dbze_grid.value,
+            "Temperature": ws.onion_t_grid.value,
+        },
+        attrs={
+            "unit": "log10(FWC/[kg/m3])",
+            "description": "Radar onion inversion table for FWC",
+        },
+    )
     ds_table = ds_table.interp(
         dBZ=np.arange(-35, 35, 1),  # Extend the dBZ range
         method="linear",
         kwargs={"fill_value": "extrapolate"},
     )
-    ds_table['Temperature'].attrs['unit'] = 'K'
-    ds_table['dBZ'].attrs['unit'] = 'dBZ'
+    ds_table["Temperature"].attrs["unit"] = "K"
+    ds_table["dBZ"].attrs["unit"] = "dBZ"
     return ds_table
 
-#%%
+
+# %%
 def plot_table(ds_table):
     plt.figure()
-    ds_table.sel(radiative_properties="FWC").isel(Temperature=[0,-1]).plot(
-                x="dBZ",
-                hue="Temperature",
-                label="log10(FWC) [kg/m3]",
-                ls='',
-                marker='.',
-            )
+    ds_table.sel(radiative_properties="FWC").isel(Temperature=[0, -1]).plot(
+        x="dBZ",
+        hue="Temperature",
+        label="log10(FWC) [kg/m3]",
+        ls="",
+        marker=".",
+    )
     plt.grid()
     plt.show()
+
 
 if __name__ == "__main__":
     # %% Create the inversion table
     for habit in ["LargePlateAggregate", "8-ColumnAggregate"]:  # Habit to use
         for psd in ["DelanoeEtAl14", "FieldEtAl07TR", "FieldEtAl07ML"]:  # PSD to use
-            
+
             # Create the inversion table
             ws = make_onion_invtable(habit, psd)
 
-            #% Create the data array with the inversion table
+            # % Create the data array with the inversion table
             ds_table = get_ds_table(habit, psd, ws)
 
             # plot_table(ds_table)
 
-            #% Save the table to a netCDF file
+            # % Save the table to a netCDF file
             ds_table.to_netcdf(
                 f"../data/onion_invtables/onion_invtable_{habit}_{psd}.nc",
                 encoding={
@@ -138,7 +151,7 @@ if __name__ == "__main__":
     # %% take a sample earthcare dataset
     # path_earthcare = "../data/earthcare/arts_x_data/"
     # ds_earthcare = xr.open_dataset(path_earthcare + "arts_x_01162E.nc")
-    
+
     # %% interpolate the table to the earthcare dataset
     # profile_fwc_log10 = ds_table.sel(radiative_properties="FWC").interp(
     #     Temperature=ds_earthcare.temperature,
