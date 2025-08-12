c44a12ae485b29f5b7286c07b548392cdb1fddb5
Anqi Li <li.a.titech@gmail.com>
2025-08-12 16:30:03 +0200

    explore scattering database

---
+++ scripts/explore_scattering_db.py
@@ -0,0 +1,20 @@
+#%%
+import os
+import pyarts.xml as xml
+
+# %%
+datafolder = "/data/s5/scattering_data/IRDbase/Yang2016/ArtsFormat"
+habit = '5-PlateAggregate-Smooth'
+
+#%%
+M=xml.load(os.path.join(datafolder, f"{habit}.meta.xml"), search_arts_path=False)
+attributes = [attr for attr in dir(M[0]) if not callable(getattr(M[0], attr)) and not attr.startswith("__")]
+print(attributes)
+print(len(M))
+# %%
+S=xml.load(os.path.join(datafolder, f"{habit}.xml"), search_arts_path=False)
+attributes = [attr for attr in dir(S[0]) if not callable(getattr(S[0], attr)) and not attr.startswith("__")]
+print(attributes)
+print(len(S))
+
+# %%
