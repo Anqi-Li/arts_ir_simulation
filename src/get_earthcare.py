1f0a4fb4265f6904521d266eac8d140799242a9f
Anqi Li <li.a.titech@gmail.com>
2025-10-28 15:43:50 +0100

    adapted to the latest ecio changes

--- src/get_earthcare.py
+++ src/get_earthcare.py
@@ -1,40 +1,21 @@
 # %%
-# import xarray as xr
-# from combine_CPR_MSI import (
-#     get_cpr_msi_from_orbits,
-#     read_xmet,
-#     xr_vectorized_height_interpolation,
-#     align_xmet_horizontal_grid,
-# )
-# from search_orbit_files import get_common_orbits
-# from tqdm import tqdm
 from pathlib import Path
-# from physics.unitconv import (
-#     mixing_ratio2mass_conc,
-#     dry_air_density,
-#     specific_humidity2h2o_p,
-# )
-
-# import os
-# from concurrent.futures import ProcessPoolExecutor, as_completed
 import numpy as np
 from ectools import ecio
 import data_paths as dp
 import os
 from scipy.interpolate import NearestNDInterpolator
 
-#
 # %% fetch all arts output files
-
-def load_and_merge_input_data(
-    frame_code, prodmod_code="ECA_EXBA", save_results=False, cfmr_vars=None, get_xmet_temperature=False
-):
+# TODO: use the new function in ecio to merge C-FMR and M-RGR data
+def load_and_merge_input_data(frame_code, product_baseline="BA", save_results=False, cfmr_vars=None, get_xmet_temperature=False):
     """Load and merge C-FMR and M-RGR data for a given orbit frame code."""
     # load C-FMR data
     ds_cfmr = ecio.load_CFMR(
         srcpath=dp.CFMR,
-        prodmod_code=prodmod_code,
-        frame_code=frame_code,
+        product_baseline=product_baseline,
+        frame=frame_code[-1],
+        orbit=frame_code[:-1],
         nested_directory_structure=True,
     )
     ds_cfmr.close()
@@ -43,8 +24,9 @@ def load_and_merge_input_data(
     if get_xmet_temperature:
         ds_xmet = ecio.load_XMET(
             srcpath=dp.XMET,
-            prodmod_code="ECA_EXAA",
-            frame_code=frame_code,
+            product_baseline="AA",
+            frame=frame_code[-1],
+            orbit=frame_code[:-1],
             nested_directory_structure=True,
         )
         ds_xmet.close()
@@ -58,21 +40,18 @@ def load_and_merge_input_data(
         )
         print("Merging XMET data into input dataset (C-FMR) done.")
 
-
-    ds_cfmr = ds_cfmr.set_coords(
-        ["longitude", "latitude", "time", "height", "surface_elevation"]
-    )
+    ds_cfmr = ds_cfmr.set_coords(["longitude", "latitude", "time", "height", "surface_elevation"])
     if cfmr_vars is not None:
         if get_xmet_temperature and "temperature" not in cfmr_vars:
             cfmr_vars.append("temperature")
         ds_cfmr = ds_cfmr[cfmr_vars]
-    
 
     # load M-RGR data
     ds_mrgr = ecio.load_MRGR(
         srcpath=dp.MRGR,
-        prodmod_code=prodmod_code,
-        frame_code=frame_code,
+        product_baseline=product_baseline,
+        frame=frame_code[-1],
+        orbit=frame_code[:-1],
         nested_directory_structure=True,
     )
     ds_mrgr.close()
@@ -87,9 +66,7 @@ def load_and_merge_input_data(
         flatten_hcoords_mrgr.data.T,
         np.arange(len(flatten_hcoords_mrgr["horizontal_grid"])),
     )
-    nearest_indices_on_flatten_hcoords_mrgr = NearestIndex(
-        np.array([ds_cfmr["longitude"], ds_cfmr["latitude"]]).T
-    ).astype(int)
+    nearest_indices_on_flatten_hcoords_mrgr = NearestIndex(np.array([ds_cfmr["longitude"], ds_cfmr["latitude"]]).T).astype(int)
     ds_mrgr_TIR_select = (
         ds_mrgr[["TIR1", "TIR2", "TIR3"]]
         .stack({"horizontal_grid": ["along_track", "across_track"]})
@@ -98,10 +75,8 @@ def load_and_merge_input_data(
     print("Selecting nearest M-RGR TIR2 pixel for each CPR ray done.")
 
     # merge datasets
-    ds_ec = ds_cfmr.assign(
-        {"msi": (("band", "along_track"), ds_mrgr_TIR_select.to_array().data)}
-    )
-    ds_ec['msi'].attrs = ds_mrgr.TIR1.attrs
+    ds_ec = ds_cfmr.assign({"msi": (("band", "along_track"), ds_mrgr_TIR_select.to_array().data)})
+    ds_ec["msi"].attrs = ds_mrgr.TIR1.attrs
     ds_ec = ds_ec.assign_coords({"band": ["TIR1", "TIR2", "TIR3"]})
     ds_ec.attrs.update(
         {
@@ -128,24 +103,28 @@ def load_and_merge_input_data(
     # print("Merging ARTS result with dataset C-FMR and M-RGR done.")
     return ds_ec
 
-#%%
-if __name__ == "__main__":
-    from earthcare_ir import Habit, PSD
-    
-    def get_arts_output_files(orbit_frame, habit, psd):
-        import data_paths as dp
-        import os
-        import glob
-        path_pattern = os.path.join(
-            dp.arts_output_TIR2,
-            f'arts_TIR2_{orbit_frame}_{habit}_{psd}.nc'
-        )
-        return glob.glob(path_pattern)
 
-    arts_file_paths = get_arts_output_files('*', Habit.Bullet, PSD.D14)
-    orbit_frames = [f.split('/')[-1].split('_')[2] for f in arts_file_paths]
-    #%%
-    for orbit_frame in orbit_frames:
+# %%
+if __name__ == "__main__":
+    # %% orbit frames based on arts output files
+    # from earthcare_ir import Habit, PSD
+
+    # def get_arts_output_files(orbit_frame, habit, psd):
+    #     import data_paths as dp
+    #     import os
+    #     import glob
+
+    #     path_pattern = os.path.join(dp.arts_output_TIR2, f"arts_TIR2_{orbit_frame}_{habit}_{psd}.nc")
+    #     return glob.glob(path_pattern)
+
+    # arts_file_paths = get_arts_output_files("*", Habit.Column, PSD.D14)
+    # orbit_frames = [f.split("/")[-1].split("_")[2] for f in arts_file_paths]
+
+    # %% get common orbit frames in August 2025
+    orbit_frames = dp.get_common_orbit_frame_list(year="2025", month="08", day="*")
+
+    # %%
+    for orbit_frame in orbit_frames[:2]:
         print(f"Processing orbit frame: {orbit_frame}")
         save_path = os.path.join(
             dp.MRGR_TIR_aligned,
@@ -153,211 +132,10 @@ if __name__ == "__main__":
         )
         if Path(save_path).exists():
             print(f"File {save_path} already exists, skipping.")
-            continue
+            # continue
         ds = load_and_merge_input_data(
             frame_code=orbit_frame,
-            save_results=True,
+            save_results=False,
             cfmr_vars=["reflectivity_corrected"],
             get_xmet_temperature=True,
         )
-# %%
-# def prepare_arts(
-#     orbit_frame,
-#     remove_clearsky=True,
-#     lowest_dBZ_threshold=-30,
-#     vars_xmet=[
-#         "temperature",
-#         "pressure",
-#         "specific_humidity",
-#         "ozone_mass_mixing_ratio",
-#         "specific_cloud_liquid_water_content",
-#         "specific_rain_water_content",
-#     ],
-#     height_grid=np.arange(0, 20e3, 100),
-# ):
-
-#     # load CPR and MSI data
-#     xds = get_cpr_msi_from_orbits(
-#         orbit_numbers=orbit_frame,
-#         msi_band=5,
-#         get_xmet=False,
-#         add_dBZ=True,
-#         remove_underground=True,
-#     )
-#     if remove_clearsky:
-#         # mask clearsky profiles
-#         mask_clearsky = (xds["dBZ"] < lowest_dBZ_threshold).all(dim="nbin")
-#         xds = xds.where(~mask_clearsky.compute(), drop=True)
-
-#     # % load xmet data
-#     ds_xmet = align_xmet_horizontal_grid(
-#         read_xmet(orbit_number=orbit_frame),
-#         xds,
-#     )
-
-#     # % Interpolate the data to a uniform height grid
-#     da_dBZ_height = xr_vectorized_height_interpolation(
-#         ds=xds,
-#         height_name="binHeight",
-#         variable_name="dBZ",
-#         height_grid=height_grid,
-#         height_dim="nbin",
-#         new_height_dim="height_grid",
-#     )
-
-#     ds_xmet["log_pressure"] = ds_xmet["pressure"].pipe(np.log)
-#     _vars_xmet = [v for v in vars_xmet if v != "pressure"] + ["log_pressure"]
-#     ds_xmet_height = xr_vectorized_height_interpolation(
-#         ds=ds_xmet,
-#         height_name="geometrical_height",
-#         variable_name=_vars_xmet,
-#         height_grid=height_grid,
-#         height_dim="height",
-#         new_height_dim="height_grid",
-#     )
-#     ds_xmet_height["log_pressure"] = ds_xmet_height["log_pressure"].interpolate_na(
-#         dim="height_grid",
-#         method="linear",
-#         fill_value="extrapolate",
-#     )
-#     ds_xmet_height["pressure"] = ds_xmet_height["log_pressure"].pipe(np.exp)
-
-#     ds_xmet_height["temperature"] = ds_xmet_height["temperature"].interpolate_na(
-#         dim="height_grid",
-#         method="nearest",
-#         fill_value="extrapolate",
-#     )
-#     # % assign attributes to the interpolated variables
-#     for v in vars_xmet:
-#         ds_xmet_height[v] = ds_xmet_height[v].assign_attrs(ds_xmet[v].attrs)
-
-#     # make H2O VMR from specific_humidity
-#     ds_xmet_height["h2o_volume_mixing_ratio"] = (
-#         (
-#             specific_humidity2h2o_p(
-#                 ds_xmet_height["specific_humidity"],
-#                 ds_xmet_height["pressure"],
-#             )
-#             / ds_xmet_height["pressure"]
-#         )
-#         .fillna(0)
-#         .assign_attrs(
-#             dict(
-#                 long_name="Water Vapor Volume Mixing Ratio",
-#                 units="kg kg-1",
-#             )
-#         )
-#     )
-
-#     # make liquid water content
-#     ds_xmet_height["density"] = dry_air_density(
-#         ds_xmet_height["pressure"],
-#         ds_xmet_height["temperature"],
-#     )
-#     ds_xmet_height["density"] = ds_xmet_height["density"].assign_attrs(
-#         dict(
-#             long_name="Air Density",
-#             units="kg m-3",
-#         )
-#     )
-
-#     ds_xmet_height["cloud_liquid_water_content"] = mixing_ratio2mass_conc(
-#         ds_xmet_height["specific_cloud_liquid_water_content"],  # kg/kg
-#         ds_xmet_height["pressure"],  # Pa
-#         ds_xmet_height["temperature"],  # K
-#     ).assign_attrs(
-#         dict(
-#             long_name="Cloud Liquid Water Content",
-#             units="kg m-3",
-#         )
-#     )
-
-#     ds_xmet_height["cloud_liquid_water_content"] = ds_xmet_height[
-#         "cloud_liquid_water_content"
-#     ].where(
-#         ds_xmet_height["cloud_liquid_water_content"] >= 0,
-#         0,  # ensure no negative values
-#     )
-
-#     ds_xmet_height["rain_water_content"] = mixing_ratio2mass_conc(
-#         ds_xmet_height["specific_rain_water_content"],  # kg/kg
-#         ds_xmet_height["pressure"],  # Pa
-#         ds_xmet_height["temperature"],  # K
-#     ).assign_attrs(
-#         dict(
-#             long_name="Rain Liquid Water Content",
-#             units="kg m-3",
-#         )  # kg/m^3 = kg/kg * kg/m^3
-#     )
-#     ds_xmet_height["rain_water_content"] = ds_xmet_height["rain_water_content"].where(
-#         ds_xmet_height["rain_water_content"] >= 0,
-#         0,  # ensure no negative values
-#     )
-#     vars_xmet = [
-#         v.replace(
-#             "specific_cloud_liquid_water_content", "cloud_liquid_water_content"
-#         ).replace("specific_rain_water_content", "rain_water_content")
-#         for v in vars_xmet
-#     ] + [
-#         "h2o_volume_mixing_ratio",
-#         "density",
-#     ]
-
-#     # % filter bad pixels, cleary sky, etc.
-#     # mask extreme MSI values
-#     mask_bad_msi = np.logical_or(
-#         (xds["pixel_values"] > 500), (xds["pixel_values"] < 150)
-#     )
-
-#     mask = ~mask_bad_msi
-
-#     # % combine all necessary variables
-#     ds = (
-#         xds[["surfaceElevation", "pixel_values"]]
-#         .assign(
-#             dBZ=da_dBZ_height,
-#             **{
-#                 v: (da_dBZ_height.dims, ds_xmet_height[v].data, ds_xmet_height[v].attrs)
-#                 for v in vars_xmet
-#             },
-#         )
-#         .where(mask.compute(), drop=True)
-#     )
-
-#     return ds
-
-
-# %%
-# if __name__ == "__main__":
-    # # % get common orbits
-    # orbit_list = get_common_orbits(["CPR", "MSI", "XMET"])
-
-    # # %%
-    # def process_orbit(orbit_frame):
-    #     path_save = os.path.join(
-    #         os.path.dirname(os.path.dirname(__file__)),
-    #         "data/earthcare/arts_input_data/",
-    #     )
-    #     filename_save = f"arts_input_{orbit_frame}.nc"
-    #     filepath = Path(path_save) / filename_save
-    #     if filepath.exists():
-    #         return f"File {filename_save} already exists, skipping."
-    #     ds = prepare_arts(orbit_frame, remove_clearsky=True)
-    #     ds.reset_index("nray").set_xindex("time").to_netcdf(
-    #         path_save + filename_save,
-    #         compute=True,
-    #     )
-    #     return f"Processed {filename_save}"
-
-    # with ProcessPoolExecutor(max_workers=64) as executor:
-    #     futures = {
-    #         executor.submit(process_orbit, orbit_frame): orbit_frame
-    #         for orbit_frame in orbit_list
-    #     }
-    #     for f in tqdm(
-    #         as_completed(futures), total=len(futures), desc="Processing orbit"
-    #     ):
-    #         result = f.result()
-    #         print(result)
-
-# %%
