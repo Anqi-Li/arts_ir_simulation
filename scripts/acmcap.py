4ee8e49cac9a98fdb0bf76e5cb790377771b6559
Anqi Li <li.a.titech@gmail.com>
2025-11-10 10:03:58 +0100

    normalised psd and acm_cap retrieval of N0star

--- scripts/acmcap.py
+++ scripts/acmcap.py
@@ -15,19 +15,14 @@ from analysis import (
 from earthcare_ir import Habit, PSD
 import pandas as pd
 
-# %%
-
-ds_acm = (
-    ecio.load_ACMCAP(
-        srcpath=dp.ACMCAP,
-        product_baseline="BA",
-        orbit="06356",
-        frame="H",
-        nested_directory_structure=True,
-    )
-    .swap_dims(along_track="time")
-    .reset_coords()
-)
+# %% load and merge datasets
+ds_acm = ecio.load_ACMCAP(
+    srcpath=dp.ACMCAP,
+    product_baseline="BA",
+    orbit="06356",
+    frame="H",
+    nested_directory_structure=True,
+).reset_coords()
 ds_acm.close()
 ds_acm = ds_acm.rename({"height": "height_acm", "MSI_longwave_channel": "band"})
 
@@ -40,14 +35,14 @@ ds_arts_ec.close()
 
 # merge acm and arts datasets
 ds_arts_re = ds_arts_ec.swap_dims(along_track="time").reindex_like(
-    ds_acm, method="nearest", tolerance=pd.Timedelta("1s")
+    ds_acm.swap_dims(along_track="time"), method="nearest", tolerance=pd.Timedelta("1s")
 )
 ds = ds_arts_re.merge(
-    ds_acm,
+    ds_acm.swap_dims(along_track="time"),
     overwrite_vars=[
         "latitude",
         "longitude",
-        "along_track",
+        # "along_track",
     ],
 ).swap_dims(time="along_track")
 ds.encoding = ds_acm.encoding
@@ -69,11 +64,11 @@ ds.encoding = ds_acm.encoding
 # %%
 ds["residual_acmcap"] = get_bias(
     ds=ds,
-    var_name1="MSI_longwave_brightness_temperature",
-    var_name2="MSI_longwave_brightness_temperature_forward",
+    var_name1="MSI_longwave_brightness_temperature_forward",
+    var_name2="MSI_longwave_brightness_temperature",
 )
-ds["residual_arts"] = get_bias(ds=ds, var_name1="msi", var_name2="arts")
-ds["residual_xgb"] = get_bias(ds=ds, var_name1="msi", var_name2="xgb")
+ds["residual_arts"] = get_bias(ds=ds, var_name1="arts", var_name2="msi")
+ds["residual_xgb"] = get_bias(ds=ds, var_name1="xgb", var_name2="msi")
 
 # %%
 nrows = 2
@@ -122,24 +117,222 @@ ecplot.plot_EC_1D(
     ax=ax[1, 0],
     ds=ds,
     plot1D_dict=plot1D_dict,
-    ylabel="MSI - X (K)",
+    ylabel="X - MSI (K)",
     title="Residuals of TIR2 Brightness Temperature",
     include_ruler=False,
 )
 
 ax[1, 0].axhline(0, color="k", linestyle="--", linewidth=1)
+
+ecplot.add_marble(ax[0, 0], ds)
 plt.tight_layout()
 
-# %% N0prime
+# %% Onion inversion table
+from onion_table import get_ds_onion_invtable
 
-nrows = 1
+fwcs = []
+for psd in [PSD.D14, PSD.F07T, PSD.MDG]:
+    for habit in [Habit.Column]:
+        ds_onion_table = get_ds_onion_invtable(
+            habit=habit,
+            psd=psd,
+            coef_mgd=(
+                {
+                    "n0": 1e10,  # Number concentration
+                    "ga": 1.5,  # Gamma parameter
+                    "mu": 0,  # Default value for mu
+                }
+                if psd == PSD.MDG
+                else None
+            ),
+        )
+        fwc = (
+            ds_onion_table.sel(radiative_properties="FWC")
+            .interp(
+                Temperature=ds["temperature"],
+                dBZ=ds.sel(psd=psd, habit=habit)["reflectivity_corrected"],
+            )
+            .pipe(lambda x: 10**x)  # Convert from log10(FWC) to FWC in kg/m^3
+            .fillna(0)
+            .expand_dims(["psd", "habit"])
+        )
+        fwcs.append(fwc)
+
+fwc = xr.concat(fwcs, dim="psd").assign_attrs(ds["ice_water_content"].attrs)
+ds = ds.assign({"ice_water_content_onion": fwc})
+
+# %% compare IWC
+fig, ax = plt.subplots(
+    nrows=4, ncols=1, figsize=(25, 7 * 4), squeeze=False, sharex=False
+)
+
+ecplot.plot_EC_2D(
+    ax=ax[0, 0],
+    ds=ds,
+    varname="ice_water_content",
+    heightvar="height_acm",
+    label="ACMCAP IWC",
+    units="kg/m$^3$",
+)
+
+for i, psd in enumerate([PSD.D14, PSD.F07T, PSD.MDG]):
+    ecplot.plot_EC_2D(
+        ax=ax[i + 1, 0],
+        ds=ds.sel(psd=psd, habit=Habit.Column),
+        varname="ice_water_content_onion",
+        heightvar="height",
+        label=psd + " IWC",
+        title=f"IWC from Onion Inversion Table - {psd}",
+    )
+
+
+ax[-1, 0].axhline(0, color="k", linestyle="--", linewidth=1)
+ecplot.add_marble(ax[0, 0], ds)
+plt.tight_layout()
+
+# for i in range(4):
+#     ax[i, 0].axvline(ds["time"].sel(along_track=along_track).data, color="k", linestyle="--")
+#     ax[i, 0].axhline(height, color="k", linestyle="--")
+
+# %% N0prime and d0star calculation
+from get_psd import get_d0star_from_n0star_iwc
+
+
+ds["d0star"] = ds.pipe(
+    lambda x: get_d0star_from_n0star_iwc(
+        x["ice_water_content"],
+        x["ice_normalized_number_concentration"],
+    )
+).assign_attrs(
+    {
+        "long_name": "Normalized median diameter",
+        "units": "m",
+        "plot_range": np.array([1e-5, 1e-2]),
+        "plot_scale": "logarithmic",
+    }
+)
+
+
+# %% Plot IWC, N0star, d0star
+nrows = 3
 fig, ax = plt.subplots(
     nrows=nrows, ncols=1, figsize=(25, 7 * nrows), squeeze=False, sharex=False
 )
 
 ecplot.plot_EC_2D(
     ax=ax[0, 0],
-    ds=ds_acm,
-    varname="ice_N0prime",
-    label="N0'",
+    ds=ds,
+    varname="ice_water_content",
+    heightvar="height_acm",
+    label="IWC",
 )
+
+ecplot.plot_EC_2D(
+    ax=ax[1, 0],
+    ds=ds,
+    varname="ice_normalized_number_concentration",
+    heightvar="height_acm",
+    label="N0*",
+)
+
+ecplot.plot_EC_2D(
+    ax=ax[2, 0],
+    ds=ds,
+    varname="d0star",
+    heightvar="height_acm",
+    label="d0*",
+)
+ecplot.add_marble(ax[0, 0], ds)
+
+ax[0, 0].axvline(ds["time"].isel(along_track=3000).data, color="k", linestyle="--")
+ax[0, 0].axhline(10000, color="k", linestyle="--")
+
+# %% Plot PSD at a specific time and height
+
+from get_psd import get_psd, psd_F05_norm
+from physics.unitconv import dgeo2deq
+
+along_track = ds.isel(along_track=2900).along_track.data.item()
+height = 8e3  # m
+
+fig, ax = plt.subplots(1,2, figsize=(25, 10))
+a, b, rho = 0.21, 2.26, 1000  # values in plate aggregate from database
+d_max = np.logspace(-6, -2, 100)  # m
+d_meq, ddmeq_ddmax = dgeo2deq(d_max, a=a, b=b, rho=rho)  # m
+
+t = (
+    (
+        ds["temperature"]
+        .sel(along_track=along_track)
+        .dropna("CPR_height")
+        .swap_dims(CPR_height="height")
+        .sel(height=height, method="nearest")
+    )
+    .load()
+    .data
+)  # K
+
+for psd in [PSD.D14, PSD.F07T, PSD.MDG, "ACM_CAP"]:
+    if psd == PSD.F07M or psd == PSD.F07T or psd == PSD.MDG:
+        d = d_max
+    else:
+        d = d_meq
+
+    if psd == "ACM_CAP":
+        d0star = (
+            ds.set_coords("height_acm")["d0star"]
+            .sel(along_track=along_track)
+            .swap_dims(JSG_height="height_acm")
+            .sel(height_acm=height, method="nearest")
+        ).data  # m
+        n0star = (
+            ds.set_coords("height_acm")["ice_normalized_number_concentration"]
+            .sel(along_track=along_track)
+            .swap_dims(JSG_height="height_acm")
+            .sel(height_acm=height, method="nearest")
+        ).data  # m^-4
+        iwc = (
+            ds.set_coords("height_acm")["ice_water_content"]
+            .sel(along_track=along_track)
+            .swap_dims(JSG_height="height_acm")
+            .sel(height_acm=height, method="nearest")
+        ).data  # kg/m^3
+        psd_data = n0star * psd_F05_norm(d_meq / d0star)  # m^-4
+    else:
+        iwc = (
+            ds.set_coords("height")["ice_water_content_onion"]
+            .sel(along_track=along_track, psd=psd, habit=Habit.Column)
+            .where(~ds.height.sel(along_track=along_track).isnull().load(), drop=True)
+            .swap_dims(CPR_height="height")
+            .sel(height=height, method="nearest")
+        ).load()  # .data  # kg/m^3
+        _, psd_data = get_psd(
+            fwc=np.atleast_1d(iwc),  # kg/m^3
+            t=np.atleast_1d(t),  # K
+            psd=psd,
+            psd_size_grid=d,
+            mgd_coef={"n0": 1e10, "ga": 1.5, "mu": 0},
+            rho=rho,
+        )
+        psd_data = psd_data.value.squeeze()  # m^-4
+
+    # make sure all psd are in terms of d_meq
+    if psd == PSD.F07T or psd == PSD.F07M or psd == PSD.MDG:
+        psd_data *= ddmeq_ddmax
+    ax[0].loglog(d_meq, psd_data, marker=".", label=f'{psd}, IWC={iwc:.2e} kg/m^3')
+    ax[1].semilogx(d_meq, psd_data*d_meq**6, marker=".", label=psd)
+
+ax[0].set_title(
+    f"PSD at along_track index {along_track}\nand height {height*1e-3} km\nT={t:.1f} K"
+)
+ax[0].set_xlabel("D")
+ax[0].set_ylabel("N(D) (m^-4)")
+ax[0].set_ylim(1e2, 1e12)
+ax[0].legend()
+
+ax[1].set_title('6th moment')
+ax[1].set_xlabel("D")
+ax[1].set_ylabel("N(D)*D^6")
+ax[1].legend()
+
+plt.tight_layout()
---
+++ scripts/psd_normalised.py
@@ -0,0 +1,245 @@
+# %%
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from earthcare_ir import PSD
+from get_psd import get_psd
+from physics.unitconv import deq2dgeo, dgeo2deq
+
+
+# %%
+def psd_F05(dmeq, t, iwc):
+    n0star = get_n0star_from_temperature(t=t)
+    d0star = get_d0star_from_n0star_iwc(n0star=n0star, iwc=iwc)
+    x = dmeq / d0star
+    return n0star * psd_F05_norm(x)
+
+def psd_F05_norm(x):
+    # Following Field et al. (2005) Table 2 first row
+    lambda_0 = 20.78
+    lambda_1 = 3.29
+    nu = 0.6357
+    return (490.6 * np.exp(-lambda_0 * x) + 17.46 * (x**nu) * np.exp(-lambda_1 * x))
+    
+
+def get_n0star_from_temperature(t):
+    # Following Mason et al 2023 Table 1
+    t_c = t - 273.15
+    a_v = np.exp(-6.9 + 0.0315 * t_c)
+    n0prime = get_n0prime_from_temperature(t)
+    return n0prime * a_v**0.6
+
+def get_n0prime_from_temperature(t):
+    t_c = t - 273.15
+    return np.exp(16.118 - 0.1303 * t_c)
+
+def get_d0star_from_n0star_iwc(iwc, n0star):
+    # Compute d0star from n0star and iwc
+    # Following Delanoë et al. (2014) Eq. (9)
+    # Using the relation: iwc = (pi/6) * rho * n0star * d0star^4 * C
+    # where C is a constant, arbitrarily chosen by Testud et al. (2001) to be equal to gamma(4)/4^4
+
+    rho = 1000  # kg/m^3  # used in Delanoë et al. (2014)
+    # C = math.gamma(4) / 4**4  # constant from Testud et al. (2001)
+    C = 1
+    d0star = (6 * iwc / (np.pi * rho * n0star * C)) ** (1 / 4)
+    return d0star
+
+
+# % Verify that integrating the PSD returns the correct IWC
+rho = 1e3
+iwc = 0.01 * 1e-3
+t = 220  # K
+d_meq = np.logspace(-6, -2, 10001)  # melted equivalent spherical diameter
+
+psd_data = psd_F05(d_meq, t=t, iwc=iwc)  # m^-4
+
+# test consistency
+iwc_from_psd_integral_dmeq = np.trapezoid(np.pi * rho / 6 * d_meq**3 * psd_data, d_meq)  # kg/m^3
+print(f"IWC from PSD integration (using dmeq): {iwc_from_psd_integral_dmeq:.2e} kg/m^3")
+
+# test consistency using dmax
+a,b = 480,3
+d_max, ddmeq_ddmax = deq2dgeo(d_meq, a=a, b=b, rho=rho)  # m
+iwc_from_psd_integral_dmax = np.trapezoid(a * d_max**b * psd_data * ddmeq_ddmax, d_max)  # kg/m^3
+print(f"IWC from PSD integration (using dmax): {iwc_from_psd_integral_dmax:.2e} kg/m^3")
+
+# show original IWC
+print(f"IWC given: {iwc:.2e} kg/m^3")
+
+# %%
+def get_psd_moment_trapz(order, psd_data, size_grid, axis=-1):
+    """Moment using trapezoidal rule.
+    order: moment order
+    psd_data: PSD data array
+    size_grid: size grid array
+    axis: axis along which to compute the moment
+    """
+    integrand = psd_data * size_grid**order
+    return np.trapezoid(integrand, size_grid, axis=axis)
+
+
+def get_n0star(i, j, psd_data, size_grid):
+    # Compute the normalized number concentration
+    # N0/N0star = normalised F(x)
+    n0star = get_psd_moment_trapz(i, psd_data, size_grid) ** (
+        (j + 1) / (j - i)
+    ) * get_psd_moment_trapz(j, psd_data, size_grid) ** ((i + 1) / (i - j))
+    return n0star
+
+
+def get_d0star(i, j, psd_data, size_grid):
+    # Compute the normalized median diameter
+    # x = d/d0star
+    d0star = (
+        get_psd_moment_trapz(i, psd_data, size_grid)
+        / get_psd_moment_trapz(j, psd_data, size_grid)
+    ) ** (-1 / (j - i))
+    return d0star
+
+#%% Two plots side by side: normal and normalised PSD
+
+a,b,rho = 0.21, 2.26, 1000  # values in plate aggregate from database
+d_max = np.logspace(-6, -2, 100)  # m
+d_meq, ddmeq_ddmax = dgeo2deq(d_max, a=a, b=b, rho=rho)  # m
+t = 250  # K
+iwc = 1e-4  # kg/m^3
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 4))
+
+for psd in [PSD.D14, PSD.F07T, PSD.MDG]:
+    if psd == PSD.F07M or psd == PSD.F07T or psd == PSD.MDG:
+        d = d_max
+    else:
+        d = d_meq
+
+    _, psd_data = get_psd(
+        fwc=np.atleast_1d(iwc),  # kg/m^3
+        t=np.atleast_1d(t),  # K
+        psd=psd,
+        mgd_coef={"n0": 1e10, "ga": 1.5, "mu": 0},
+        scat_species_a=a,
+        scat_species_b=b,
+        psd_size_grid=d,
+        rho=rho,
+    )
+    psd_data = psd_data.value.squeeze()  # m^-4
+
+    # make sure all psd are in terms of d_meq
+    if psd == PSD.F07T or psd == PSD.F07M or psd == PSD.MDG:
+        psd_data *= ddmeq_ddmax
+
+    # Left plot: Normal PSD
+    ax1.loglog(d_meq, psd_data.squeeze(), marker=".", markersize=1, linewidth=0.5, label=psd)
+
+    # Right plot: Normalized PSD
+    n0star = get_n0star(i=2, j=3, psd_data=psd_data, size_grid=d_meq)
+    d0star = get_d0star(i=2, j=3, psd_data=psd_data, size_grid=d_meq)
+    psd_data_norm = psd_data / n0star
+    x = d_meq / d0star
+    ax2.semilogy(x, psd_data_norm.squeeze(), marker=".", markersize=1, linewidth=0.5, label=psd)
+    print(psd)
+    iwc_from_psd_integral = np.trapezoid(np.pi*rho/6 * d_meq**3 * psd_data, d_meq)
+    print(
+        f"IWC from psd integration over dmeq: {iwc_from_psd_integral:.3e} kg/m^3"
+    )
+    print('ratio to given IWC:', iwc_from_psd_integral/iwc)
+    iwc_from_psd_integral = np.trapezoid(a * d_max**b * psd_data / ddmeq_ddmax, d_max)
+    print(
+        f"IWC from psd integration over dmax: {iwc_from_psd_integral:.3e} kg/m^3"
+    )
+    print('ratio to given IWC:', iwc_from_psd_integral/iwc)
+
+    ax3.plot(d_meq, psd_data.squeeze()*d_meq**6, marker=".", markersize=1, linewidth=0.5, label=psd)
+
+# ACMCAP for both plots
+psd_data = psd_F05(d_meq, t=t, iwc=iwc)  # m^-4
+ax1.loglog(d_meq, psd_data, marker=".", markersize=1, linewidth=0.5, label="ACM_CAP")
+
+x = d_meq / get_d0star_from_n0star_iwc(
+    n0star=get_n0star_from_temperature(t=t), iwc=iwc
+)
+ax2.semilogy(x, psd_F05_norm(x), marker=".", markersize=1, linewidth=0.5, label="ACM_CAP")
+print("ACM_CAP")
+iwc_from_psd_integral = np.trapezoid(np.pi*rho/6 * d_meq**3 * psd_data, d_meq)
+print(
+    f"IWC from psd integration over dmeq: {iwc_from_psd_integral:.3e} kg/m^3"
+)
+print('ratio to given IWC:', iwc_from_psd_integral/iwc)
+iwc_from_psd_integral = np.trapezoid(a * d_max**b * psd_data / ddmeq_ddmax, d_max)
+print(
+    f"IWC from psd integration over dmax: {iwc_from_psd_integral:.3e} kg/m^3"
+)
+print('ratio to given IWC:', iwc_from_psd_integral/iwc)
+
+ax3.plot(d_meq, psd_data.squeeze()*d_meq**6, marker=".", markersize=1, linewidth=0.5, label="ACM_CAP")
+
+# Figure formatting
+fig.suptitle(f"IWC={iwc:.2e} kg/m^3, T={t:.1f} K \na={a:.3f}, b={b:.3f}")
+
+# Left plot formatting
+ax1.set_xlabel("$D_{meq}$ (m)")
+ax1.set_ylabel("$N(D)$ (m$^{-4}$)")
+ax1.set_title(f"PSD")
+ax1.set_ylim(1e-0, 1e12)
+ax1.legend()
+ax1.grid()
+
+# Right plot formatting
+ax2.set_xlabel("$x = D_{meq}/D_0^*$")
+ax2.set_ylabel("$N(D_{meq})/N_0^*$")
+ax2.set_title("Normalized PSD")
+ax2.set_ylim(1e-5, 1e3)
+ax2.set_xlim(0, 5)
+ax2.legend()
+ax2.grid()
+
+ax3.set_xlabel("$D_{meq}$ (m)")
+ax3.set_ylabel("$N(D) \cdot D_{meq}^6$ (m$^{2}$)")
+ax3.set_title(f"6th Moment of PSD")
+ax3.legend()
+ax3.grid()
+
+plt.tight_layout()
+plt.show()
+
+
+#%% test 3rd and 4th moment of normalised psd equal to IWC
+
+#%% test changing n0star effects on ACM_CAP PSD
+t_c = np.linspace(0, -80, 100) 
+t = t_c+ 273.15
+
+fig, ax = plt.subplots(1,2, figsize=(10,4))
+ax[0].plot(get_n0star_from_temperature(t), t_c)
+ax[0].set_xscale('log')
+ax[0].set_xlabel('N0star (m^-4)')
+ax[0].set_ylabel('Temperature (°C)')
+ax[0].invert_yaxis()
+ax[0].grid()
+
+ax[1].plot(get_n0prime_from_temperature(t), t_c)
+ax[1].set_xscale('log')
+ax[1].set_xlabel("N0prime (m^-4)")
+ax[1].set_ylabel('Temperature (°C)')
+ax[1].invert_yaxis()
+ax[1].grid()
+plt.show()
+
+t_c = [0, -20, -40, -60]  # °C
+t = np.array(t_c) + 273.15
+iwc = 1e-4  # kg/m^3
+n0star_values = [get_n0star_from_temperature(ti) for ti in t]
+d_meq = np.logspace(-6, -2, 100)  # m
+for i, n0star in enumerate(n0star_values):
+    d0star = get_d0star_from_n0star_iwc(n0star=n0star, iwc=iwc)
+    x = d_meq / d0star
+    psd_cap = n0star * psd_F05_norm(x)
+    plt.loglog(d_meq, psd_cap, label=f'T={t_c[i]} °C, N0*={n0star:.2e}, D0*={d0star*1e3:.2f} mm')
+
+plt.ylim(1e0, 1e12)
+plt.xlabel('D_meq (m)')
+plt.ylabel('N(D) (m^-4)')
+plt.title(f'ACM_CAP PSD for different temperatures \nat IWC={iwc:.2e} kg/m^3')
+plt.legend()
+plt.grid()
